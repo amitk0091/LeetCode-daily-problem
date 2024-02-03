02b3c80b2a4368fbea39c7a473e3b5ed6fb07ab3
Amit kumawat <97072335+amitk0091@users.noreply.github.com>
2024-02-03 11:54:12 +0530

    Time: 10 ms (80.66%) | Memory: 10.8 MB (23.07%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
-    int help(int i, int k, vector<int>& arr,vector<int> &dp ){
-        if(i==arr.size()) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int mx=INT_MIN;
-        int res=0;
-        for(int j=i;j<=(i+(k-1)) && j<arr.size();j++){
-            mx=max(mx,arr[j]);
-            res = max(res,(j-i+1)*mx + help(j+1,k,arr,dp));
+    int help(int i, int k, vector<int>& arr, vector<int>& dp) {
+        if (i == arr.size())
+            return 0;
+        if (dp[i] != -1)
+            return dp[i];
+        int mx = INT_MIN;
+        int res = 0;
+        for (int j = i; j <= (i + (k - 1)) && j < arr.size(); j++) {
+            mx = max(mx, arr[j]);
+            res = max(res, (j - i + 1) * mx + help(j + 1, k, arr, dp));
         }
-        return dp[i]=res;
+        return dp[i] = res;
     }
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
-        vector<int> dp(arr.size(),-1);
-        return help(0,k,arr,dp);
+        // vector<int> dp(arr.size(),-1);
+        // return help(0,k,arr,dp);
+        int dp[501];
+        int n = arr.size();
+        fill_n(dp, n, 0);
+
+        for (int i = n - 1; i >= 0; i--) {
+            int mx = INT_MIN;
+            int res = 0;
+            for (int j = i; j <= (i + (k - 1)) && j < n; j++) {
+                mx = max(mx, arr[j]);
+                res = max(res, (j - i + 1) * mx + (j+1<n ? dp[j + 1] : 0));
+            }
+            dp[i] = res;
+        }
+        return dp[0];
     }
 };
