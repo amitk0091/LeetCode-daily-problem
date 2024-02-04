cfa844ede55740b8dc101f3659c080c35e605dc8
Amit kumawat <97072335+amitk0091@users.noreply.github.com>
2024-02-04 22:14:58 +0530

    Time: 23 ms (31.59%) | Memory: 10.5 MB (9.91%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int i = 0, j = 0;
-        unordered_map<char, int> m1;
-        for (char& ch : t)
-            m1[ch]++;
-        
-        unordered_map<char, int> m2;
-        int cnt = 0, ans = INT_MAX;
-        pair<int, int> ind = {-1, -1};
-
+        int i = 0, j = 0, cnt = 0, ans = INT_MAX;
+        unordered_map<char, int> m1, m2;
+        for (char& ch : t) m1[ch]++;
+        pair<int,int> ind={-1,-1};
         while (j < s.length()) {
             m2[s[j]]++;
-            if (m1.find(s[j]) != m1.end() && m2[s[j]] == m1[s[j]])
-                cnt++;
+            if (m1.find(s[j])!=m1.end() && m2[s[j]] == m1[s[j]]) cnt++;
             if (cnt == m1.size()) {
-                while (i < s.length() && (m1.find(s[i]) == m1.end() || m1[s[i]] < m2[s[i]])) {
+                while (i< s.length() && (m1.find(s[i])==m1.end() || m1[s[i]] < m2[s[i]])) {
                     m2[s[i]]--;
                     i++;
                 }
-                if (ans > (j - i + 1)) {
-                    ans = j - i + 1;
-                    ind = {i, j};
+                if(ans>(j-i+1)){
+                    ans=j-i+1;
+                    ind= {i,j};
                 }
                 m2[s[i]]--;
                 cnt--;
@@ -29,6 +23,6 @@ public:
             }
             j++;
         }
-        return (ind.first == -1) ? "" : s.substr(ind.first, ans);
+        return (ind.first==-1 ? "":s.substr(ind.first,ans));
     }
 };
