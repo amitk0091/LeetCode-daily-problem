0e653d41159d9809e6e2e628b60bf4eb9783d2ee
Amit kumawat <97072335+amitk0091@users.noreply.github.com>
2024-02-05 13:19:40 +0530

    Time: 11 ms (99.40%) | Memory: 11.9 MB (27.34%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        unordered_map<char,int> m;
         int freq[26];
         for(int i=0;i<s.length();i++) {
-            char ch = s[i];
-            if(freq[ch-'a']==0) freq[ch-'a']=i+1;
-            else freq[ch-'a']=-1;
+            if(freq[s[i]-'a']==0) freq[s[i]-'a']=i+1;
+            else freq[s[i]-'a']=-1;
         }
         int ind = INT_MAX;
         for(int i=0;i<26;i++){
