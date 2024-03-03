637365db36fc83658fa4ed92c46f4f2ea7405fa8
Amit kumawat <97072335+amitk0091@users.noreply.github.com>
2024-03-04 03:29:37 +0530

    Time: 50 ms (40.00%) | Memory: 41 MB (60.00%) - LeetSync

---
+++ 3335-minimum-operations-to-write-the-letter-y-on-a-grid/minimum-operations-to-write-the-letter-y-on-a-grid.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int minimumOperationsToWriteY(vector<vector<int>>& grid) {
+        vector<int> cnty(3);
+            vector<int> remain(3);
+        int n = grid.size();
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(i==j && i<=n/2){
+                    cnty[grid[i][j]]++;
+                }
+                else if(i==(n-1-j) && i<=n/2){
+                    cnty[grid[i][j]]++;
+                }
+                else if(i>n/2 && j==n/2) cnty[grid[i][j]]++;
+                else remain[grid[i][j]]++;
+            }
+            
+        }
+        
+        int op =INT_MAX;
+        // 0 // 1
+        op = min(cnty[1]+cnty[2] + remain[0]+ remain[2] , op); 
+        
+        //0 2
+        op = min(cnty[1]+cnty[2] + remain[0]+ remain[1] , op);
+        
+        // 1 0 
+        op = min(cnty[0]+cnty[2] + remain[1]+ remain[2] , op);
+        
+        // 1 2
+        op = min(cnty[0]+cnty[2] + remain[0]+ remain[1] , op);  
+        
+        // 2 0 
+        op = min(cnty[0]+cnty[1] + remain[2]+ remain[1] , op);
+        
+        // 2 1
+        op = min(cnty[0]+cnty[1] + remain[0]+ remain[2] , op);
+
+        return op;
+    }
+};
