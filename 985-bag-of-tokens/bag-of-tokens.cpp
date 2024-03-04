37efcaabde3e22598e5315b072044b7aba717b94
Amit kumawat <97072335+amitk0091@users.noreply.github.com>
2024-03-04 14:18:35 +0530

    Time: 4 ms (74.87%) | Memory: 13 MB (55.08%) - LeetSync

--- 985-bag-of-tokens/bag-of-tokens.cpp
+++ 985-bag-of-tokens/bag-of-tokens.cpp
@@ -5,14 +5,9 @@ public:
         int n = tokens.size();
         if(n==0) return 0;
         sort(tokens.begin(),tokens.end());
-        for(int i=0;i<n;i++){
-            cout<<tokens[i]<<" ";
-        }
-        cout<<endl;
         int i=0,j =n-1,score=0;
 
         while( i <= j){
-            cout<<i<<" "<<j<<" "<<score<<" "<<power<<endl;
             if(power >= tokens[i]){
                 power-=tokens[i];
                 score++;
