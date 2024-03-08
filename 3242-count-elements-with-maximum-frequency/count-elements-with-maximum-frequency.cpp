69b3c26800632da12c867d0737ef0025f6288818
Amit kumawat <97072335+amitk0091@users.noreply.github.com>
2024-03-08 05:45:53 +0530

    Time: 3 ms (84.14%) | Memory: 21.6 MB (22.65%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
     int maxFrequencyElements(vector<int>& nums) {
-        map<int,int> m;
-        int mxFreq =0;
-        for(int& num : nums){
-            m[num]++;
-            mxFreq = max(mxFreq,m[num]);
+        int mxFreq =0,cnt=0,ans=0;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            cnt++;
+            if(i==nums.size()-1 || nums[i]!=nums[i+1]){
+                if(cnt==mxFreq) ans+=cnt;
+                else if(cnt>mxFreq) ans= cnt;
+                mxFreq = max(mxFreq,cnt);
+                cnt = 0;
+            }
         }
-        int cnt=0;
-        for(auto [key,value] : m){
-            if(value==mxFreq) cnt+=mxFreq;
-        }
-        return cnt;
+        return ans;
+        // map<int,int> m;
+        // for(int& num : nums){
+        //     m[num]++;
+        //     mxFreq = max(mxFreq,m[num]);
+        // }
+        // int cnt=0;
+        // for(auto [key,value] : m){
+        //     if(value==mxFreq) cnt+=mxFreq;
+        // }
+        // return cnt;
     }
 };
