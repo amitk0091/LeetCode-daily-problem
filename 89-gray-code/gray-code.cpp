ec4c9c2e7fd7c34b1fdac840f2e995781e51a0f7
Amit kumawat <97072335+amitk0091@users.noreply.github.com>
2024-03-09 16:00:44 +0530

    Time: 2 ms (85.76%) | Memory: 12.1 MB (92.86%) - LeetSync

---
+++ 89-gray-code/gray-code.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        // gray code  i xor (half of i)
+        vector<int> ans (1<<n);
+        for(int i=0;i< (1<<n);i++){
+            ans[i]=(i^(i>>1));
+        }
+        return ans;
+    }
+};
