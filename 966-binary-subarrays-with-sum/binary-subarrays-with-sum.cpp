3f0eb279fe874183d8cbcfa6372e470bfc4d29ff
Amit kumawat <97072335+amitk0091@users.noreply.github.com>
2024-03-14 11:14:28 +0530

    Time: 65 ms (21.27%) | Memory: 39 MB (16.91%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -2,20 +2,17 @@ class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int target) {
         int n = nums.size();
-        unordered_map<int,int> m;
+        map<int,int> m;
         vector<int> prefixZeroCnt(n);
         int cnt=0;
         m[0] =-1;
         int sum =0,ans=0;
         for(int i=0;i<n;i++){
            sum+=nums[i];
-           if(nums[i]==0) cnt++;
-           else  cnt=0;
+           cnt = (nums[i]==0 ? cnt+1 : 0);
            prefixZeroCnt[i] = cnt;
-           cout<<sum-target<<endl;
            if(m.find(sum - target) != m.end()){
              ans += 1+ (m[sum-target]==-1 ? 0: prefixZeroCnt[m[sum-target]]);
-             cout<<"ans "<<ans<<endl;
            }
            m[sum] = i; 
         }
