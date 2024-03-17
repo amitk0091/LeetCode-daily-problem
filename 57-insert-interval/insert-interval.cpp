1613d72d62c32fe1a5b9d18538a25ff5967ca60f
Amit kumawat <97072335+amitk0091@users.noreply.github.com>
2024-03-17 12:51:07 +0530

    Time: 15 ms (31.27%) | Memory: 20.4 MB (82.99%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -2,23 +2,33 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& it, vector<int>& nt) {
         vector<vector<int>> ans;
-        it.push_back(nt);
-        sort(it.begin(),it.end());
-        int start = -1,end=-1;
-        for(int i=0;i<it.size();i++){
-           if(end<it[i][0]){
-             if(end!=-1){
-                ans.push_back({start,end});
-             }
-             start = it[i][0];
-             end = it[i][1];
-           }
-           else end = max(end,it[i][1]);
-
-           if(i==it.size()-1){
-            ans.push_back({start,end});
-           }
-        //    cout<<start<<" "<<end<<endl;
+        // it.push_back(nt);
+        // sort(it.begin(),it.end());
+        // int start = it[0][0],end=it[1][0];
+        // for(int i=1;i<it.size();i++){
+        //    if(end<it[i][0]){
+        //         ans.push_back({start,end});
+        //      }
+        //      start = it[i][0];
+        //      end = it[i][1];
+        //    }
+        //    else end = max(end,it[i][1]);
+        // }
+        // ans.push_back({start,end});
+        int i=0,n= it.size();
+        while(i<n  && it[i][1]<nt[0]){
+            ans.push_back(it[i]);
+            i++;
+        }
+        while(i<n && it[i][0] <= nt[1]){
+            nt[1] = max(nt[1],it[i][1]);
+            nt[0] = min(nt[0],it[i][0]);
+            i++;
+        }
+        ans.push_back(nt);
+        while(i<n){
+            ans.push_back(it[i]);
+            i++;
         }
         return ans;
     }
