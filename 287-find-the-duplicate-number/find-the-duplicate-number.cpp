fd51dff7e6466653f6902fb4518f2ab04c15cf75
Amit kumawat <97072335+amitk0091@users.noreply.github.com>
2024-03-24 07:46:57 +0530

    Time: 82 ms (55.90%) | Memory: 63.7 MB (56.88%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -3,12 +3,28 @@ class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
         int  n = nums.size();
-        int arr[100005]={0};
-        for(int i=0;i<n;i++){
-            if(arr[nums[i]]>=1) return nums[i];
-            arr[nums[i]]++;
+
+        int slow = nums[0];
+        int fast = nums[0];
+
+       // actually they are pointing to same so to avoid the condition first time we donot use fast
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while(slow!=fast);
+
+        slow = nums[0];
+        while(slow!=fast){
+           slow = nums[slow];
+            fast = nums[fast]; 
         }
-        return -1;
+        return slow;
+        // int arr[100005]={0};
+        // for(int i=0;i<n;i++){
+        //     if(arr[nums[i]]>=1) return nums[i];
+        //     arr[nums[i]]++;
+        // }
+        // return -1;
 
         // ll sum = accumulate(nums.begin(),nums.end(),0);
         // return (sum-n*(n-1)/2);
