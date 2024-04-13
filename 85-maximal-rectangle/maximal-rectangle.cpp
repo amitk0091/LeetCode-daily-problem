a3639544aa0b2e84482f599b77610c37958735d9
Amit kumawat <97072335+amitk0091@users.noreply.github.com>
2024-04-13 14:07:16 +0530

    Time: 29 ms (76.08%) | Memory: 17.6 MB (67.14%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -6,7 +6,7 @@ public:
         int area = 0;
         for (int i = 0; i < h.size(); i++) {
             while (!st.empty() && h[st.top()] >= h[i]) {
-                int k = st.top(),width = 1;
+                int k = st.top(), width = 1;
                 st.pop();
                 if (!st.empty()) {
                     width += (k - st.top() - 1);
@@ -17,25 +17,21 @@ public:
                 area = max(area, width * h[k]);
             }
             st.push(i);
-            if (i == h.size() - 1) {
-                while (!st.empty()) {
-                    int k = st.top(),width = h.size() - k;
-                    st.pop();
-                    if (!st.empty()) {
-                        width += (k - st.top() - 1);
-                    } else {
-                        width += (k);
-                    }
-                    area = max(area, width * h[k]);
-                }
-            }
         }
-        cout << area << endl;
+        while (!st.empty()) {
+            int k = st.top(), width = h.size() - k;
+            st.pop();
+            if (!st.empty()) {
+                width += (k - st.top() - 1);
+            } else {
+                width += (k);
+            }
+            area = max(area, width * h[k]);
+        }
         return area;
     }
     int maximalRectangle(vector<vector<char>>& matrix) {
         // easy pizzy
-
         // at each level we have to find largest rectangle in histogram
         int ans = 0;
         vector<int> h(matrix[0].size());
