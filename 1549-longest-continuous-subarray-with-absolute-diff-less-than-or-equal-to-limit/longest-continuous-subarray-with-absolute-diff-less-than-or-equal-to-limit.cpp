bc9eb8802687e2a21b420dc608c36897de245a7a
Amit kumawat <97072335+amitk0091@users.noreply.github.com>
2024-04-26 15:45:03 +0530

    Time: 69 ms (67.74%) | Memory: 54.4 MB (79.21%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -2,21 +2,37 @@ class Solution {
 public:
     int longestSubarray(vector<int>& nums, int limit) {
         int n = nums.size();
-        map<int, int> m;
-        int mx = 0, mn = INT_MAX;
+        // map<int, int> m;
+        // int mx = 0, mn = INT_MAX;
+        deque<int> dqmx,dqmn;
         int j = 0,ans=1;
         for (int i = 0; i < n; i++) {
-            m[nums[i]]++;
-            mx = max(nums[i], mx);
-            mn = min(nums[i], mn);
-            while (j<=i && mx - mn > limit) {
-                m[nums[j]]--;
-                if (m[nums[j]] == 0)
-                    m.erase(nums[j]);
-                mn = m.begin()->first;
-                mx = m.rbegin()->first;
-                j++;
-            }
+            while(!dqmx.empty() && dqmx.back() < nums[i]) dqmx.pop_back();
+            while(!dqmn.empty() && dqmn.back() > nums[i]) dqmn.pop_back();
+            
+            dqmx.push_back(nums[i]);
+            dqmn.push_back(nums[i]);
+
+            // m[nums[i]]++;
+             
+           int mx = dqmx.front();
+           int mn = dqmn.front();
+           while(j<=i && mx-mn>limit){
+              if(nums[j]==dqmx.front()) dqmx.pop_front();
+              if(nums[j]==dqmn.front()) dqmn.pop_front();
+              mx = dqmx.front();
+              mn = dqmn.front();
+              j++;
+           }
+            // int mn = min(nums[i], mn);
+            // while (j<=i && mx - mn > limit) {
+            //     m[nums[j]]--;
+            //     if (m[nums[j]] == 0)
+            //         m.erase(nums[j]);
+            //     mn = m.begin()->first;
+            //     mx = m.rbegin()->first;
+            //     j++;
+            // }
             ans = max(ans,(i-j+1));
         }
         return ans;
