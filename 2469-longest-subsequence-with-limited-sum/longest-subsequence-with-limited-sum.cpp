ceea95ba8713edf94579ab9867f0a13e672d2d5e
Amit kumawat <97072335+amitk0091@users.noreply.github.com>
2024-04-27 16:00:15 +0530

    Time: 25 ms (16.54%) | Memory: 119.5 MB (5.25%) - LeetSync

---
+++ 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int smaller_and_equal(vector<int> nums, int val){
+        int low =0, high=nums.size()-1,ans=-1;
+        while(low<=high){
+            int mid = (low +high)/2;
+            if(nums[mid]<= val ){
+                ans = mid;
+                low= mid+1;
+            }
+            else high = mid-1;
+        }
+        return ans;
+    }
+    vector<int> answerQueries(vector<int>& nums, vector<int>& qs) {
+        sort(nums.begin(),nums.end());
+        for(int i=1;i<nums.size();i++){
+            nums[i]+=nums[i-1];
+        }
+        vector<int> ans;
+        for(auto q : qs){
+           ans.push_back(smaller_and_equal(nums,q)+1);
+        }
+        return ans;
+    }
+};
