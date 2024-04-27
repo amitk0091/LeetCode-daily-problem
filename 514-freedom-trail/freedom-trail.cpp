777ca5503f829547459dcbf694a9963c11a6faf0
Amit kumawat <97072335+amitk0091@users.noreply.github.com>
2024-04-27 09:53:34 +0530

    Time: 120 ms (27.36%) | Memory: 73.1 MB (20.29%) - LeetSync

---
+++ 514-freedom-trail/freedom-trail.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int dp[101][101];
+    int help(int i,int pre_j, string ring, string key) {
+        if (i == key.length())
+            return 0;
+        if(dp[i][pre_j] !=-1) return dp[i][pre_j];
+        int cost = INT_MAX;
+        // cout<<i<<" i "<<endl;
+        for (int j = 0; j < ring.length(); j++) {
+            if (ring[j] == key[i]) {
+                int curCost =
+                    min((int)abs(j - pre_j) + 1,
+                        (int)(ring.length() - max(j, pre_j) + min(pre_j, j) + 1));
+                cost = min(cost, curCost + help(i + 1,j, ring, key));
+            }
+        }
+        // cout<<cost<<"  "<<i<<endl;
+        // return cost;
+        return dp[i][pre_j]=cost;
+    }
+    int findRotateSteps(string ring, string key) {
+        fill_n(&dp[0][0],101*101,-1);
+        return help(0,0, ring, key);
+    }
+};
