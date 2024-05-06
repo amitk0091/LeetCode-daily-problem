7140aebc096c40f420477557312c6a74482290b2
Amit kumawat <97072335+amitk0091@users.noreply.github.com>
2024-05-06 06:32:41 +0530

    Time: 246 ms (93.72%) | Memory: 159.7 MB (90.66%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
@@ -10,12 +10,44 @@
  */
 class Solution {
 public:
-   int mx=0;
+    int mx = 0;
+    ListNode* reverseLinkedList(ListNode* head) {
+        if (head == NULL)
+            return head;
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        while (curr) {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
     ListNode* removeNodes(ListNode* head) {
-       if(head==NULL) return NULL;
-       head->next = removeNodes(head->next);
-       mx= max(mx,head->val);
-       if(mx>head->val) return head->next;
-       return head; 
+
+        // reverse a linked list
+        if (head == NULL)
+            return NULL;
+        ListNode* newh = reverseLinkedList(head);
+        int pre = newh->val;
+        ListNode* temp = newh;
+        ListNode* prev = newh;
+        while (temp) {
+            pre = max(pre, temp->val);
+            if (temp->val < pre) {
+                prev->next = temp->next;
+            }
+            else prev = temp;
+            temp = temp->next;
+        }
+        ListNode* newH2 = reverseLinkedList(newh);
+        return newH2;
     }
 };
+
+//    if(head==NULL) return NULL;
+//    head->next = removeNodes(head->next);
+//    mx= max(mx,head->val);
+//    if(mx>head->val) return head->next;
+//    return head;
