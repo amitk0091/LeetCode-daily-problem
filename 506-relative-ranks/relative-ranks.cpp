9d67c9eec8efbcc27df0a6ae6369c6cb486fa4a1
Amit kumawat <97072335+amitk0091@users.noreply.github.com>
2024-05-09 02:46:01 +0530

    Time: 6 ms (82.21%) | Memory: 13.6 MB (82.27%) - LeetSync

--- 506-relative-ranks/relative-ranks.cpp
+++ 506-relative-ranks/relative-ranks.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     vector<string> findRelativeRanks(vector<int>& score) {
-        unordered_map<int, int> mp;
-        vector<int> s1=score;
-        sort(s1.begin(), s1.end(), greater<int>());
+        // unordered_map<int, int> mp;
+        // vector<int> s1=score;
+        // sort(s1.begin(), s1.end(), greater<int>());
+        priority_queue<pair<int,int>> pq;
         for (int i = 0; i < score.size(); i++) {
-            mp[s1[i]] = i;
+            // mp[s1[i]] = i;
+            pq.push({score[i],i});
         }
-        vector<string> st;
+        vector<string> st(score.size());
         for (int i = 0; i < score.size(); i++) {
-            if (mp[score[i]] == 0) {
-                st.push_back("Gold Medal");
-            } else if (mp[score[i]] == 1) {
-                st.push_back("Silver Medal");
-            } else if (mp[score[i]] == 2) {
-                st.push_back("Bronze Medal");
+            auto k = pq.top();
+            pq.pop();
+            if (i == 0) {
+                st[k.second]="Gold Medal";
+            } else if (i== 1) {
+                st[k.second]="Silver Medal";
+            } else if (i == 2) {
+                st[k.second]="Bronze Medal";
             } else {
-                st.push_back(to_string(mp[score[i]]+1));
+                st[k.second]= to_string(i+1);
             }
         }
         return st;
