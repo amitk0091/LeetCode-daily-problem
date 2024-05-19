babf52d25e22a26057ad344bc09629b7f6c7dff0
Amit kumawat <97072335+amitk0091@users.noreply.github.com>
2024-05-20 01:52:30 +0530

    Time: 169 ms (92.12%) | Memory: 125.7 MB (49.32%) - LeetSync

--- 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.cpp
+++ 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.cpp
@@ -5,40 +5,19 @@ class Solution {
 public:
     long long maximumValueSum(vector<int>& nums, int k,
                               vector<vector<int>>& edges) {
-        int n = nums.size();
-        vector<vector<int>> arr(n,vector<int>(3,0));
-        int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            arr[i][1] = nums[i];
-            arr[i][2] = nums[i] ^ k;
-            if (arr[i][1] < arr[i][2])
-                cnt++;
-            arr[i][0] = arr[i][2] - arr[i][1];
-            cout<<arr[i][0]<<" ";
-        }
-        cout<<endl;
-        sort(arr.begin(), arr.end());
+        int n = nums.size(),cnt = 0,diff = INT_MAX;
         ll ans = 0;
-        if (cnt % 2 == 0) {
-            for (int i = 0; i < n; i++) {
-                ans+=max(arr[i][1],arr[i][2]);
-            }
-        } else {
-            for (int i = 0; i < n; i++) {
-                if (i > 0 && arr[i][0] > 0 && arr[i - 1][0] < 0) {
-                    if (abs(arr[i][0]) > abs(arr[i - 1][0])) {
-                        ans += arr[i-1][0];
-                        ans += arr[i][2];
-                    } else
-                        ans += arr[i][1];
-                }
-                else if(i==0 && arr[i][0]>0){
-                    ans+=arr[i][1];
-                }
-                else{
-                    ans+=max(arr[i][1],arr[i][2]);
-                }
-            }
+        
+        for (int i = 0; i < n; i++) {
+            int a = nums[i];
+            int b = nums[i] ^ k;
+            if (a < b)
+                cnt++;
+            diff = min(diff, abs(b-a));
+            ans += max(a,b);
+        }
+        if(cnt%2==1){
+            ans -= diff;
         }
         return ans;
     }
