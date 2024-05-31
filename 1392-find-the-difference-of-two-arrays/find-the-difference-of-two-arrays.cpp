303eda43fdb11890169eb4c11931d2b4b238b329
Amit kumawat <97072335+amitk0091@users.noreply.github.com>
2024-06-01 05:10:49 +0530

    Time: 22 ms (97.52%) | Memory: 29.7 MB (96.32%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
@@ -1,18 +1,48 @@
 class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-        set<int> st1(nums1.begin(), nums1.end());
-        set<int> st2(nums2.begin(), nums2.end());
-        set<int> st11;
-        set<int> st22;
-        for(int i=0;i<nums1.size();i++){
-            if(st2.find(nums1[i]) == st2.end()) st11.insert(nums1[i]);
-        }
-         for(int i=0;i<nums2.size();i++){
-            if(st1.find(nums2[i]) == st1.end()) st22.insert(nums2[i]);
-        }
-        vector<int> a(st11.begin(),st11.end());
-        vector<int> b(st22.begin(),st22.end());
-        return {a,b};
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+          int i=0,j=0;
+          vector<vector<int>> ans(2);
+          while(i<nums1.size() && j<nums2.size()){
+               if(nums1[i] < nums2[j]){
+                   if(i-1>=0 && nums1[i-1] == nums1[i]){
+                    i++;
+                    continue;
+                   }
+                   ans[0].push_back(nums1[i]);
+                   i++;
+               }
+               else if(nums1[i] == nums2[j]) {
+                i++;
+                j++;
+               }
+               else{
+                  if(j-1>=0 && nums2[j] == nums2[j-1]) {
+                    j++;
+                    continue;
+                    }
+                  ans[1].push_back(nums2[j]);
+                  j++;
+               }
+          }
+          while(j<nums2.size()){
+             if(j-1>=0 && nums2[j] == nums2[j-1]) {
+                    j++;
+                    continue;
+                    }
+                  ans[1].push_back(nums2[j]);
+                  j++;
+          }
+          while(i<nums1.size()){
+            if(i-1>=0 && nums1[i-1] == nums1[i]){
+                    i++;
+                    continue;
+                   }
+                   ans[0].push_back(nums1[i]);
+                   i++;
+          }
+          return ans;
     }
 };
