dd65873cfba9a505a0cfe91a4ffee9a4b381c58d
Amit kumawat <97072335+amitk0091@users.noreply.github.com>
2024-06-01 04:36:05 +0530

    Time: 10 ms (38.10%) | Memory: 12.2 MB (98.72%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -1,13 +1,31 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        vector<int> ans;
-        for(int i=0;i<nums.size();i++){
-            if(i-1>=0 && nums[i-1] == nums[i]) continue;
-            else if(i+1<nums.size() && nums[i+1] == nums[i]) continue;
-            ans.push_back(nums[i]);
+        // sort(nums.begin(),nums.end());
+        // vector<int> ans;
+        // for(int i=0;i<nums.size();i++){
+        //     if(i-1>=0 && nums[i-1] == nums[i]) continue;
+        //     else if(i+1<nums.size() && nums[i+1] == nums[i]) continue;
+        //     ans.push_back(nums[i]);
+        // }
+        // return ans;
+
+        int overAllXOR = 0;
+        for (int& num : nums) {
+            overAllXOR ^= num;
         }
-        return ans;
+        int bitPos = 0;
+        while (((overAllXOR >> bitPos) & 1) == 0)
+            bitPos++;
+
+        int a = 0, b = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if ((nums[i] >> bitPos) & 1) {
+                a ^= nums[i];
+            } else {
+                b ^= nums[i];
+            }
+        }
+        return {a, b};
     }
 };
