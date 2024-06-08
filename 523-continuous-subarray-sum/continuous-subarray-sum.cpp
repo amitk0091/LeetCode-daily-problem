c77234473a1dadb4ef7a4b4fd8823a64d020385b
Amit kumawat <97072335+amitk0091@users.noreply.github.com>
2024-06-08 13:18:35 +0530

    Time: 171 ms (78.69%) | Memory: 134.8 MB (31.96%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        if(nums.size()<=1) return false;
          unordered_map<int,int> mp;
           int sum =0;
           mp[sum] = -1;
          for(int i=0;i<nums.size();i++){
-            if(nums[i]==0 && (i-1>=0 && nums[i-1]==0)) return true;
-            else if(nums[i]==0) continue;
             sum+=nums[i];
             if(mp.find(sum%k)!=mp.end()){
-                cout<<i<<" "<<sum<<endl;
                 if(i-mp[sum%k]>=2) return true;
             }
             else mp[sum%k] =i;
